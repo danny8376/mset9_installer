626012d34773e928ac7497a192786f2fa8be3b21
DannyAAM <danny@saru.moe>
2024-11-20 13:58:23 +0800

    confirm mset9 removal when cfw not installed

--- lib/consts.dart
+++ lib/consts.dart
@@ -6,4 +6,4 @@ const kN3dsFolder = "Nintendo 3DS";
 final kId0Regex = RegExp(r"^(?![0-9a-fA-F]{4}(01|00)[0-9a-fA-F]{18}00[0-9a-fA-F]{6})[0-9a-fA-F]{32}$");
 final kId1Regex = RegExp("^[0-9a-fA-F]{32}(?:$kOldId1Suffix)?\$");
 
-final kFirmBakRegex = RegExp(r"^firm\d_enc.bak$");
+final kFirmBakRegex = RegExp(r"^firm\d_enc\.bak$");
